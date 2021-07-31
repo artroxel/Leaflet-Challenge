905bec8b48f5fca36157ff4cfca4064e5152ddad
Amanda Troxel <amandatroxel2@gmail.com>
2021-07-31 11:50:55 -0600

    update

--- Leaflet_Step1/static/js/logic.js
+++ Leaflet_Step1/static/js/logic.js
@@ -3,7 +3,7 @@ var myMap = L.map("mapid", {
     zoom: 10
 });
 
-L.titleLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution:"© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -12,4 +12,40 @@ L.titleLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_tok
   accessToken: API_KEY
 }).addTo(myMap);
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson")
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", function(data) {
+    
+    function mapLayout(feature) {
+        return {
+            color: "#000000",
+            opacity: 2,
+            fillOpacity: 2, 
+            fillColor: chooseColor(feature.properties.size),
+            radius: circleRadius(feature.properties.size),
+            stroke: true,
+            weight: .5
+        };
+    }
+    function chooseColor(size) {
+        switch (size) {
+            case size > 90:
+                return "#CB2B3E";
+            case size > 70:
+                return "#CB8427";
+            case size > 50:
+                return "#FFD326";
+            case size > 30:
+                return "#CAC428";
+            case size > 10:
+                return "#2AAD27";
+            default:
+                return "#2C99EA";
+        }
+    }
+
+    function circleRadius(eq_mag) {
+        if (eq_mag === 0) {
+            return 1;
+        }
+        return eq_mag * 3
+    }
+})
